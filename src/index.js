b6af743ad31cc56c4874847ee828ce1fdcae7ba5
vigneshraja-g <vigneshraja20@gmail.com>
2019-08-26 13:41:23 +0530

    React boilerplate code clean

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@babel/polyfill'
+import './styles.css'
+import App from './App'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
+
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
