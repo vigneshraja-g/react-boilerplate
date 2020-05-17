3de5f3f6512a705b259785101765c8af764b85d0
vignesh-raja-ei <vignesh.raja@empowerinnovate.com>
2020-05-17 13:40:57 +0530

    MiniCssExtractPlugin config changes

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -16,7 +17,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.s[ac]ss$/i,
@@ -27,6 +28,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 };
